496a98e58a6719f4a21e1f95617e01a49249b582
Tran Duc Manh <manhtran13102004@gmail.com>
2026-06-26 11:21:31 +0700

    first commit

---
+++ src/main/java/com/hocviec/HocviecApplication.java
@@ -0,0 +1,13 @@
+package com.hocviec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HocviecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HocviecApplication.class, args);
+	}
+
+}
