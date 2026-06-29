b5cf8ab79426a071022f7f68d2ed294b6df37074
Tran Duc Manh <manhtran13102004@gmail.com>
2026-06-29 10:31:29 +0700

    Bài 15: Spring Security cơ bản

---
+++ src/main/java/com/hocviec/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.hocviec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@Configuration
+@EnableWebSecurity // Kích hoạt tính năng bảo mật web
+public class SecurityConfig {
+
+    @Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        // 1. Tắt CSRF
+        .csrf(csrf -> csrf.disable())
+        
+        // 2. Cấu hình phân quyền đường dẫn
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/swagger", "/swagger-ui/**", "/v3/api-docs/**", "/api-docs/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        
+        // 3. Sử dụng Basic Auth (Viết ngắn gọn bằng Method Reference)
+        // Lưu ý: Nhớ thêm dòng này ở trên cùng file: import org.springframework.security.config.Customizer;
+        .httpBasic(Customizer.withDefaults()); 
+
+    return http.build();
+}
+}
